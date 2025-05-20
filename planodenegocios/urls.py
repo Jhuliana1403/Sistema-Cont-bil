7bd77ca234632b152a8a756ac5e8fb93e7755e63
Jhuliana1403 <promiselie013@gmail.com>
2025-05-20 15:56:45 -0300

    Estratégias de Marketing

--- planodenegocios/urls.py
+++ planodenegocios/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='inicio'),
+    path('marketing/posicionamento', views.posicionamento, name='posicionamento'),
+    path('marketing/focosegmentacao', views.foco, name='foco'),
+    path('marketing/plano', views.plano, name='plano'),
+    path('marketing/distribuicao', views.distribuicao, name='distribuicao'),
 ]
--- planodenegocios/views.py
+++ planodenegocios/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'planodenegocios/index.html')
+
+def posicionamento(request):
+    return render(request, 'planodenegocios/posicionamento.html')
+
+def foco(request):
+    return render(request, 'planodenegocios/focosegmentacao.html')
+
+def plano(request):
+    return render(request, 'planodenegocios/plano.html')
+
+def distribuicao(request):
+    return render(request, 'planodenegocios/distribuicao.html')
