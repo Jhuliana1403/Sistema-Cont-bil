5a7f8f9702f8e659e47326dfee7cdaa755402ff7
vanessa <vv202576@gmail.com>
2025-05-20 19:56:19 -0300

    Adiciona telas de análise de setor, mercado potencial e análise da concorrência

--- planodenegocios/urls.py
+++ planodenegocios/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='inicio'),
+    path('analise-setor/', views.analise_setor, name='analise_setor'),
+    path('mercado-potencial/', views.mercado_potencial, name='mercado_potencial'),
+    path('analise-concorrencia/', views.analise_concorrencia, name='analise_concorrencia'),
 ]
--- planodenegocios/views.py
+++ planodenegocios/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'planodenegocios/index.html')
+
+def analise_setor(request):
+    return render(request, 'planodenegocios/analise_setor.html')
+
+def mercado_potencial(request):
+    return render(request, 'planodenegocios/mercado_potencial.html')
+
+def analise_concorrencia(request):
+    return render(request, 'planodenegocios/analise_concorrencia.html')
